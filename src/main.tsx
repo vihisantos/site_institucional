5d401ca6447c89070fb2aefe1c2c178778dba083
vihisantos <vitrsv462@gmail.com>
2026-01-24 14:07:36 -0300

    Fix: Switch to HashRouter for GH Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App'
 import './index.css'
@@ -12,9 +12,9 @@ applyTheme(defaultTheme);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </HelmetProvider>
   </React.StrictMode>,
 )
